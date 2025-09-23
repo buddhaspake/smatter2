5a3f455cad6baee9312151b847edfe4f01697b45
Sambuddha <da.animator@gmail.com>
2025-09-23 13:39:21 +0530

    Update homepage and model
    - Added NewsItems as Homepage children
    - Added 3 default fields to Homepage

--- smm/home/models.py
+++ smm/home/models.py
@@ -1,7 +1,41 @@
+from django.db import models
 from wagtail.fields import RichTextField
 from wagtail.models import Page
+from wagtail.images import get_image_model_string
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
-    content_panels = Page.content_panels + ["body"]
+    intent = models.CharField(max_length=700, default="intent")
+    methods = models.CharField(max_length=700, default="methods")
+    vision = models.CharField(max_length=700, default="vision")
+
+    def get_context(self, request):
+        ctx = super().get_context(request)
+        feat_news = self.get_children().type(NewsItem)
+        ctx["featured_news"] = feat_news
+        return ctx
+
+    content_panels = Page.content_panels + [
+        "intent",
+        "methods",
+        "vision",
+    ]
+
+
+class NewsItem(Page):
+    date = models.DateField("Publish date")
+    caption = models.CharField(max_length=500)
+    photo = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    featured = models.BooleanField(default=False)
+
+    content_panels = Page.content_panels + [
+        "date",
+        "caption",
+        "photo",
+        "featured",
+    ]
+
