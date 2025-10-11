c64eaa220219511947bc2db84620692a73aaed31
Sambuddha <da.animator@gmail.com>
2025-10-11 15:30:43 +0530

    Add publications templates
    - Added templates for publications page and table snippet
    - Added templatetag for publications table

--- smm/base/templatetags/snippet_tags.py
+++ smm/base/templatetags/snippet_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from base.models.snippets import NewsItem
+from base.models.snippets import NewsItem, Publication
 
 register = template.Library()
 
@@ -7,4 +7,11 @@ register = template.Library()
 @register.inclusion_tag("newsitem.html")
 def newsitem(new: NewsItem):
     return {"self": new}
+
+
+@register.inclusion_tag("publications_table.html")
+def publications_table():
+    publications = Publication.objects.all().order_by("year").reverse()
+    indices = list(range(len(publications), 0, -1))
+    return {"publications": list(zip(indices, publications))}
 
