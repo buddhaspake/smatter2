c7b2d57a63655fee63c2b7d0dba6fe0128d1fd3d
Sambuddha <da.animator@gmail.com>
2025-10-26 01:41:50 +0530

    Add basic member rendering

--- smm/base/templatetags/snippet_tags.py
+++ smm/base/templatetags/snippet_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from wagtail.images.models import Image as WagtailImage
-from base.models.snippets import NewsItem, Publication
+from base.models.snippets import NewsItem, Publication, Member
 
 register = template.Library()
 
@@ -15,6 +15,11 @@ def publications_table():
     publications = Publication.objects.all().order_by("year").reverse()
     indices = list(range(len(publications), 0, -1))
     return {"publications": list(zip(indices, publications))}
+
+
+@register.inclusion_tag("member.html")
+def member(member: Member):
+    return {"self": member}
 
 
 @register.inclusion_tag("gallery_photo.html")
